d393d4ed29d181d1876d287cf3a96f8b3a97f92c
Robbie Singer <robbiebsinger@gmail.com>
2018-12-21 22:22:27 +0000

    Add CORS

---
+++ src/cors.ts
@@ -0,0 +1,26 @@
+export default (req, res, next) => {
+  if ("OPTIONS" == req.method) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+    );
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization, Content-Length, X-Requested-With"
+    );
+    res.sendStatus(200);
+  } else {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+    );
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization, Content-Length, X-Requested-With"
+    );
+
+    next();
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import * as express from "express";
 import * as http from "http";
 import * as bodyParser from "body-parser";
 import combine from "./routes/combine";
+import cors from "./cors";
 
 const app = express();
 
-app.use(bodyParser.text());
+app.use(cors);
+app.use(bodyParser.json());
 app.use(combine);
 
 const server = http.createServer(app);
