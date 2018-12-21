bacaf59e07f2fd54ce091eee99eaaacf39bec2ce
Robbie Singer <robbiebsinger@gmail.com>
2018-12-21 22:22:47 +0000

    Remove unnecessary parse

--- src/routes/combine.ts
+++ src/routes/combine.ts
@@ -4,8 +4,8 @@ import combiner from "../combiner";
 const router = Router();
 
 router.post("/combine", async (req, res) => {
-  const parsed = JSON.parse(req.body);
-  const combined = combiner(parsed);
+  
+  const combined = combiner(req.body);
 
   res.set("Content-Type", "text/plain");
   await res.send(combined);
